5eb906c375bc0910be55afedc193f09a946926c3
Vinod Singh Rawat <vinodrwt17@gmail.com>
2025-02-19 22:43:17 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,9 @@
                 </a> -->
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-600 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-400 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-gray-800  focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-3 dark:border-gray-700 dark:text-gray-400">
+                Log in
+            </button>
         </div>
     </form>
 </x-guest-layout>
